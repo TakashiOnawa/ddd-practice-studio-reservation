8f0fa2daeeba7e5f2edef79d65059576c4377f9f
takashi_onawa <onawatakashi@gmail.com>
2019-11-05 18:10:27 +0900

    初期コード作成。

---
+++ src/main/java/org/taonaw/reservation/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package org.taonaw.reservation.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String helloWorld() {
+        return "HelloWorld";
+    }
+}
