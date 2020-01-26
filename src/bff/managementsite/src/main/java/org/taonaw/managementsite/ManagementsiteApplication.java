a07b958fd290db1990de41fbef4fc641d4bdc655
takashi_onawa <onawatakashi@gmail.com>
2020-01-26 20:36:38 +0900

    管理サイト作成。

---
+++ src/bff/managementsite/src/main/java/org/taonaw/managementsite/ManagementsiteApplication.java
@@ -0,0 +1,13 @@
+package org.taonaw.managementsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementsiteApplication.class, args);
+	}
+
+}
---
+++ src/bff/managementsite/src/test/java/org/taonaw/managementsite/ManagementsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taonaw.managementsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
