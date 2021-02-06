e5ce4d4adf6ba7dc418b1718cc6e1da253f725f2
Takashi Onawa <onawatakashi@gmail.com>
2021-02-07 00:39:24 +0900

    wip

--- src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/ServletInitializer.kt
+++ src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/ServletInitializer.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 class ServletInitializer : SpringBootServletInitializer() {
 
 	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-		return application.sources(IdentityaccessApplication::class.java)
+		return application.sources(IdentityAccessApplication::class.java)
 	}
 
 }
