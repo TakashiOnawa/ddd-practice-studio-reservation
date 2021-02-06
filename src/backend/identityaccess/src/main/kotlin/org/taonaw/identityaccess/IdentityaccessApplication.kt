4474259d222ad12fc7d42f563fd1c7b14c5fc05a
Takashi Onawa <onawatakashi@gmail.com>
2021-02-06 17:01:22 +0900

    create identityaccess project.

---
+++ src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/IdentityaccessApplication.kt
@@ -0,0 +1,11 @@
+package org.taonaw.identityaccess
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdentityaccessApplication
+
+fun main(args: Array<String>) {
+	runApplication<IdentityaccessApplication>(*args)
+}
