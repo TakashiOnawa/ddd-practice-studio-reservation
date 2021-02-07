96a061e6004f0dac2b7e48ef752764a903251159
Takashi Onawa <onawatakashi@gmail.com>
2021-02-07 12:30:16 +0900

    wip

--- src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/model/memberAccount/MemberName.kt
+++ src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/model/memberAccount/MemberName.kt
@@ -4,9 +4,20 @@ data class MemberName(
         val firstName: String,
         val lastName: String) {
 
+    companion object {
+        const val FIRST_NAME_LENGTH_MIN: Int = 1
+        const val FIRST_NAME_LENGTH_MAX: Int = 20
+        const val LAST_NAME_LENGTH_MIN: Int = 1
+        const val LAST_NAME_LENGTH_MAX: Int = 20
+    }
+
     init {
-//        Assertion.argumentRange(firstName, 1, 50)
-//        Assertion.argumentRange(lastName, 1, 50)
+        require(lastName.length in LAST_NAME_LENGTH_MIN..LAST_NAME_LENGTH_MAX) {
+            "姓 $LAST_NAME_LENGTH_MIN 以上 $LAST_NAME_LENGTH_MAX 以下でなければなりません。"
+        }
+        require(firstName.length in FIRST_NAME_LENGTH_MIN..FIRST_NAME_LENGTH_MAX) {
+            "名は $FIRST_NAME_LENGTH_MIN 以上 $FIRST_NAME_LENGTH_MAX 以下でなければなりません。"
+        }
     }
 
     fun asFormattedName(): String? {
