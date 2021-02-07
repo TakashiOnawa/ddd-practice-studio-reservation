7132f249e9ed3f7fae6821d03a328643e2cce45b
Takashi Onawa <onawatakashi@gmail.com>
2021-02-07 12:32:38 +0900

    wip

--- src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/model/staffAccount/StaffName.kt
+++ src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/model/staffAccount/StaffName.kt
@@ -1,4 +1,26 @@
 package org.taonaw.identityaccess.domain.model.staffAccount
 
-data class StaffName(val value: String) {
+data class StaffName(
+        val firstName: String,
+        val lastName: String) {
+
+    companion object {
+        const val FIRST_NAME_LENGTH_MIN: Int = 1
+        const val FIRST_NAME_LENGTH_MAX: Int = 20
+        const val LAST_NAME_LENGTH_MIN: Int = 1
+        const val LAST_NAME_LENGTH_MAX: Int = 20
+    }
+
+    init {
+        require(lastName.length in LAST_NAME_LENGTH_MIN..LAST_NAME_LENGTH_MAX) {
+            "姓 $LAST_NAME_LENGTH_MIN 以上 $LAST_NAME_LENGTH_MAX 以下でなければなりません。"
+        }
+        require(firstName.length in FIRST_NAME_LENGTH_MIN..FIRST_NAME_LENGTH_MAX) {
+            "名は $FIRST_NAME_LENGTH_MIN 以上 $FIRST_NAME_LENGTH_MAX 以下でなければなりません。"
+        }
+    }
+
+    fun asFormattedName(): String? {
+        return "$lastName $firstName"
+    }
 }
