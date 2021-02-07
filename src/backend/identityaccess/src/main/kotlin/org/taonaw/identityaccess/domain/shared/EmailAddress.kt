f6281075eba700529f306a828ceaa5cbac7ff15f
Takashi Onawa <onawatakashi@gmail.com>
2021-02-07 12:54:44 +0900

    wip

--- src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/shared/EmailAddress.kt
+++ src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/shared/EmailAddress.kt
@@ -1,4 +1,19 @@
 package org.taonaw.identityaccess.domain.shared
 
 data class EmailAddress(val value: String) {
+
+    companion object {
+        const val LENGTH_MIN = 1
+        const val LENGTH_MAX = 100
+        const val FORMAT_PATTERN = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*"
+    }
+
+    init {
+        require(value.length in LENGTH_MIN..LENGTH_MAX) {
+            "メールアドレスは $LENGTH_MIN 以上 $LENGTH_MAX 以下でなければなりません。"
+        }
+        require(Regex(FORMAT_PATTERN).matches(value)) {
+            "メールアドレスのフォーマットが不正です。"
+        }
+    }
 }
