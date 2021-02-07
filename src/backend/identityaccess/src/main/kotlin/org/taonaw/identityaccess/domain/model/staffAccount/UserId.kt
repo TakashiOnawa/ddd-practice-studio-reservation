56d42793f8cef528bb9b7550340a211c0db9b196
Takashi Onawa <onawatakashi@gmail.com>
2021-02-07 12:47:44 +0900

    wip

--- src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/model/staffAccount/UserId.kt
+++ src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/model/staffAccount/UserId.kt
@@ -1,4 +1,19 @@
 package org.taonaw.identityaccess.domain.model.staffAccount
 
 data class UserId(val value: String) {
+
+    companion object {
+        const val LENGTH_MIN = 1
+        const val LENGTH_MAX = 16
+        const val FORMAT_PATTERN = "[a-zA-Z0-9]*"
+    }
+
+    init {
+        require(value.length in LENGTH_MIN..LENGTH_MAX) {
+            "ユーザー ID は $LENGTH_MIN 以上 $LENGTH_MAX 以下でなければなりません。"
+        }
+        require(Regex(FORMAT_PATTERN).matches(value)) {
+            "ユーザー ID は半角英数でなければなりません。"
+        }
+    }
 }
