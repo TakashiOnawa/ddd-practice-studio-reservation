ac2a323e0920b81ae0c1b486e580ada04f5f42fe
Takashi Onawa <onawatakashi@gmail.com>
2021-02-07 14:04:11 +0900

    wip

--- src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/shared/PlainTextPassword.kt
+++ src/backend/identityaccess/src/main/kotlin/org/taonaw/identityaccess/domain/shared/PlainTextPassword.kt
@@ -2,6 +2,21 @@ package org.taonaw.identityaccess.domain.shared
 
 data class PlainTextPassword(private val value: String) {
 
+    companion object {
+        const val LENGTH_MIN = 8
+        const val LENGTH_MAX = 20
+        const val FORMAT_PATTERN = "[a-zA-Z0-9]*"
+    }
+
+    init {
+        require(value.length in LENGTH_MIN..LENGTH_MAX) {
+            "パスワードは $LENGTH_MIN 文字以上 $LENGTH_MAX 文字以下でなければなりません。"
+        }
+        require(Regex(FORMAT_PATTERN).matches(value)) {
+            "パスワードは半角英数でなければなりません。"
+        }
+    }
+
     fun hash(passwordHashingService: PasswordHashingService): HashedPassword {
         return passwordHashingService.hash(value)
     }
